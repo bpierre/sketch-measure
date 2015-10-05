e31bb884082efa77f7a698e49ebff4840ff92fac
Pierre Bertet <bonjour@pierrebertet.net>
2015-10-05 12:47:32 +0100

    Ubuntu Color Names

--- Sketch Measure.sketchplugin/Contents/Sketch/color-names.js
+++ Sketch Measure.sketchplugin/Contents/Sketch/color-names.js
@@ -1,4 +1,19 @@
 var ColorNames = {
-    // '#ffffff': 'White',
-    // '#5d5d5d': 'Dark Grey',
+
+    // Greys
+    '#111111': 'Jet',
+    '#3b3b3b': 'Inkstone',
+    '#5d5d5d': 'Slate',
+    '#666666': 'Graphite',
+    '#888888': 'Ash',
+    '#cdcdcd': 'Silk',
+    '#f7f7f7': 'Porcelain',
+    '#ffffff': 'White',
+
+    // Colors
+    '#19b6ee': 'Blue',
+    '#3eb34f': 'Green',
+    '#ed3146': 'Red',
+    '#e95420': 'Orange',
+    '#f99b0f': 'Yellow',
 };
